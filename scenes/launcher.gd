a86df79a72558d84c22533928391b2852a614119
Qwatrum <171450552+Qwatrum@users.noreply.github.com>
2024-08-25 12:35:47 +0200

    Added more stuff

--- scenes/launcher.gd
+++ scenes/launcher.gd
@@ -1,5 +1,6 @@
 extends Window
 
+@onready var main := get_parent()
 var bar_opened = false
 
 func _ready():
@@ -25,3 +26,9 @@ func _on_back_pressed():
 	$"Buttons".show()
 	$"CreditsText".hide()
 	$"Back".hide()
+
+
+func _on_launch_pressed():
+	bar_opened = true
+	main.add_bar()
+	queue_free()
--- scenes/main.gd
+++ scenes/main.gd
@@ -1,8 +1,13 @@
 extends Node
 
+@onready var bar = preload("res://scenes/bar.tscn")
 @onready var launcher = preload("res://scenes/launcher.tscn")
 
 func _ready():
 	get_viewport().transparent_bg = true
 	var launcher_inste = launcher.instantiate()
 	add_child(launcher_inste)
+
+func add_bar():
+	var bar_inste = bar.instantiate()
+	add_child(bar_inste)
